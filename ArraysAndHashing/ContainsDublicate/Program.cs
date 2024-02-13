2d22909a08b5f1f20edc5fa4528468704c56b739
Georgi <georgi.ginduzov17d@gmail.com>
2024-02-13 23:56:32 +0200

    Duplicate char checker

---
+++ ArraysAndHashing/ContainsDublicate/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ContainsDublicate
+{
+    internal class Program
+    {
+        static bool ContainsDublicate(int[] nums)
+        {
+            Dictionary<int, int> uniqueNumbers = new Dictionary<int, int>();
+            int i = 0;
+            
+            foreach (int num in nums)
+            {
+                if (!uniqueNumbers.ContainsValue(num))
+                {
+                    uniqueNumbers.Add(i, num);
+                    i++;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+             return false;
+           
+        }
+        static void Main(string[] args)
+        {
+            int[] inputArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            Console.WriteLine(ContainsDublicate(inputArray));
+        }
+    }
+}

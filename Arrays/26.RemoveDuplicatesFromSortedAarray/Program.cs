769887386c2810a3130386dbc66265df654ca024
Georgi <georgi.ginduzov17d@gmail.com>
2024-07-09 20:37:31 +0300

    Remove duplicates

---
+++ Arrays/26.RemoveDuplicatesFromSortedAarray/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _26.RemoveDuplicatesFromSortedAarray
+{
+    internal class Program
+    {
+        public static int RemoveDuplicates(int[] nums)
+        {
+            HashSet<int> uniqueNumbers = new HashSet<int>(nums.Length);
+
+            foreach (int n in nums)
+            {
+                uniqueNumbers.Add(n);
+            }
+
+            nums = new int[uniqueNumbers.Count];
+
+            int k = 0;
+            foreach (int num in uniqueNumbers)
+            {
+                nums[k++] = num;
+            }
+
+            return uniqueNumbers.Count;
+        }
+
+        static void Main(string[] args)
+        {
+            RemoveDuplicates(new int[] { 1, 1, 2 });
+            RemoveDuplicates(new int[] { 1, 1, 2 });
+
+        }
+    }
+}

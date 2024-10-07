5553156f2430edff0115ff154319d2ee4d5b8bbf
Georgi <123997331+Georgi-Ginduzov@users.noreply.github.com>
2024-10-07 08:13:54 +0300

    Time: 46 ms (53.59%) | Memory: 37.2 MB (30.39%) - LeetSync

--- 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.cs
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.cs
@@ -1,13 +1,10 @@
 public class Solution {
     public int BalancedStringSplit(string s) {
         int substrings = 0;
-        for(int i = 0; i < s.Length; i++)
+        int balance = s[0] == 'L' ? -1 : 1;
+        for(int i = 1; i < s.Length; i++)
         { 
-            int balance = s[i] == 'L' ? -1 : 1;
-            while(i < s.Length - 1 && balance != 0)
-            {
-                balance += s[++i] == 'L' ? -1 : 1;
-            }
+            balance += s[i] == 'L' ? -1 : 1;
             if(balance == 0)
             {
                 substrings++;

8eaa951524879ec194e147e9b0d16214803a9a7d
Georgi <123997331+Georgi-Ginduzov@users.noreply.github.com>
2024-12-15 12:29:52 +0200

    Time: 0 ms (100.00%) | Memory: 42.6 MB (50.00%) - LeetSync

---
+++ 3484-lexicographically-smallest-string-after-a-swap/lexicographically-smallest-string-after-a-swap.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public string GetSmallestString(string s) {
+        char[] num = s.ToCharArray();
+        for(int i = 0; i < num.Length - 1; i++)
+        {
+            if(num[i] % 2 == num[i+1] % 2 && num[i] > num[i+1])
+            {
+                (num[i],num[i+1]) = (num[i+1],num[i]);
+                break;
+            }
+        }
+        
+        return new string(num);
+    }
+}

c0beac88cdaffae2691838dcecb5c712b7fde150
Georgi <123997331+Georgi-Ginduzov@users.noreply.github.com>
2024-12-16 15:03:00 +0200

    Time: 7 ms (22.09%) | Memory: 43.9 MB (7.45%) - LeetSync

---
+++ 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.cs
@@ -0,0 +1,8 @@
+public class Solution {
+    public int FindFinalValue(int[] nums, int original) {
+        HashSet<int> set = new HashSet<int>(nums);
+        while(set.Contains(original))
+            original *= 2;
+        return original;
+    }
+}

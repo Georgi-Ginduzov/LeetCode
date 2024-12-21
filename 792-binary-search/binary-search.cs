c202ea71182ffaa0a608ebb052b4f85333aa867f
Georgi <123997331+Georgi-Ginduzov@users.noreply.github.com>
2024-12-21 18:32:14 +0200

    Time: 0 ms (100.00%) | Memory: 52.2 MB (16.59%) - LeetSync

--- 792-binary-search/binary-search.cs
+++ 792-binary-search/binary-search.cs
@@ -2,15 +2,15 @@ public class Solution {
     public int Search(int[] nums, int target) {
         int low = 0;
         int high = nums.Length-1;
-        while(low <= high)
+        while(low <=high)
         {
-            int mid = low + (high - low)/2;
-            if(nums[mid] == target)
+            int mid = low + (high-low)/2;
+            if(nums[mid]==target)
                 return mid;
             else if(nums[mid] > target)
-                high = mid - 1;
+                high = mid-1;
             else
-                low = mid + 1;
+                low =mid+1;
         }
         return -1;
     }

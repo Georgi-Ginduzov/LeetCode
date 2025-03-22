364fa05f901e80db10a7a9d999687b12da1662bf
Georgi <123997331+Georgi-Ginduzov@users.noreply.github.com>
2025-03-22 13:45:34 +0200

    Time: 0 ms (100.00%) | Memory: 42.3 MB (76.45%) - LeetSync

---
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int FindKthPositive(int[] arr, int k) {
+        int low = 0;
+        int high = arr.Length;
+        while(low < high)
+        {
+            int mid = low + (high-low) / 2;
+            if(arr[mid] - mid - 1 < k)
+                low = mid + 1;
+            else
+                high = mid;
+        }
+
+        return high + k;
+    }
+}

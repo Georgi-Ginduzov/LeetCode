35ad80cbe2461e37666374161f89560c7efb2654
Georgi <123997331+Georgi-Ginduzov@users.noreply.github.com>
2025-03-30 14:45:25 +0300

    Time: 0 ms (100.00%) | Memory: 48.3 MB (5.45%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cs
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cs
@@ -1,22 +1,18 @@
 public class Solution {
     public char NextGreatestLetter(char[] letters, char target) {
-        int low = 0;
-        int high = letters.Length - 1;
-
-        if(letters[high] <= target)
+        if(letters[letters.Length-1] <= target)
             return letters[0];
+        
+        int low = 0; 
+        int high = letters.Length-1;
         while(low < high)
         {
-            int mid = low + (high-low) /2;
-            if(letters[mid] <= target)
-            {
-                low = mid+1;
-            }
-            else
-            {
+            int mid = low + (high-low) / 2;
+            if(letters[mid] > target)
                 high = mid;
-            }
+            else
+                low = mid + 1;
         }
-        return letters[low];
+        return letters[high];
     }
 }

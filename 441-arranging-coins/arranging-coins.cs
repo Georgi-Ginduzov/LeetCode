73496aa12921c49dcf68464cbe9d059f7beeec01
Georgi <123997331+Georgi-Ginduzov@users.noreply.github.com>
2025-04-02 22:33:14 +0300

    Time: 0 ms (100.00%) | Memory: 29.2 MB (43.12%) - LeetSync

--- 441-arranging-coins/arranging-coins.cs
+++ 441-arranging-coins/arranging-coins.cs
@@ -1,20 +1,19 @@
 public class Solution {
     public int ArrangeCoins(int n) {
-        long low = 1;
+        long low = 0;
         long high = n;
-        while(low <= high)
+        while(low < high)
         {
-            long mid = low + (high-low)/2;
-            if(mid * (mid + 1) / 2 > n)
-            {
-                high = mid - 1;
-            }
+            long mid = low + (high-low) / 2;
+            if((mid * (mid + 1)) / 2 >= n)
+                high = mid;
             else
-            {
                 low = mid + 1;
-            }
         }
-        
-        return (int)high;
+
+        if(high * (high + 1) / 2 <= n)
+            return (int)high;
+        else
+            return (int)(high - 1);
     }
 }

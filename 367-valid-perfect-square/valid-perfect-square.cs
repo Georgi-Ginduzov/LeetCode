c743c518b4ed5735e67372b6cdf8fa374df93d67
Georgi <123997331+Georgi-Ginduzov@users.noreply.github.com>
2025-04-05 16:48:06 +0300

    Time: 0 ms (100.00%) | Memory: 31 MB (40.16%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.cs
+++ 367-valid-perfect-square/valid-perfect-square.cs
@@ -1,20 +1,20 @@
 public class Solution {
     public bool IsPerfectSquare(int num) {
+        if(num == 1)
+            return true;
         long low = 0;
-        long high = num;
-
+        long high = num / 2;
         while(low < high)
         {
             long mid = low + (high-low) / 2;
-            if(mid * mid >= num)
+            if(mid*mid >= num)
                 high = mid;
             else
                 low = mid + 1;
         }
-        
-        if(high*high == num)
+
+        if(low*low == num)
             return true;
-        else
-            return false;
+        return false;
     }
 }

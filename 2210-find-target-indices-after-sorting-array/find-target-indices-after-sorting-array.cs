0765e244b2d77ecddf5703bfae985c8e3811c228
Georgi <123997331+Georgi-Ginduzov@users.noreply.github.com>
2025-04-05 18:55:43 +0300

    Time: 3 ms (87.23%) | Memory: 47.9 MB (9.93%) - LeetSync

--- 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.cs
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.cs
@@ -1,14 +1,26 @@
 public class Solution {
     public IList<int> TargetIndices(int[] nums, int target) {
         Array.Sort(nums);
-        List<int> indices = new List<int>();
-        for(int i = 0; i < nums.Length && nums[i] <= target; i++)
+
+        int low = 0;
+        int high = nums.Length - 1;
+        while(low < high)
         {
-            if(nums[i] == target)
-            {
-                indices.Add(i);
-            }
+            int mid = low + (high-low) / 2;
+            if(nums[mid] >= target)
+                high = mid;
+            else
+                low = mid+1;
         }
-        return indices;
+
+        List<int> res = new List<int>();
+
+        if(nums[high] != target)
+            return res;
+        
+        for(int i = high; i < nums.Length && nums[i] == target; i++)
+            res.Add(i);
+
+        return res;
     }
 }

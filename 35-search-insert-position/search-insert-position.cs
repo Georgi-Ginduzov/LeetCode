fc366ad251565923b2ed415143ee3ea3f559df24
Georgi <123997331+Georgi-Ginduzov@users.noreply.github.com>
2025-04-05 13:01:26 +0300

    Time: 0 ms (100.00%) | Memory: 42.9 MB (69.54%) - LeetSync

--- 35-search-insert-position/search-insert-position.cs
+++ 35-search-insert-position/search-insert-position.cs
@@ -1,24 +1,19 @@
 public class Solution {
     public int SearchInsert(int[] nums, int target) {
-        if(nums[0] >= target)
-            return 0;
-        else if(nums[nums.Length - 1] < target)
+        if(nums[nums.Length-1] < target)
             return nums.Length;
         
-        int low = 0;
-        int high = nums.Length;
+        int low = 0; 
+        int high = nums.Length - 1;
         while(low < high)
         {
-            int mid = low + (high - low) / 2;
-            if(nums[mid] < target)
-            {
-                low = mid + 1;
-            }
-            else
-            {
+            int mid = low + (high-low) / 2;
+            if(nums[mid] >= target)
                 high = mid;
-            }
+            else
+                low = mid + 1;
         }
+
         return high;
     }
 }

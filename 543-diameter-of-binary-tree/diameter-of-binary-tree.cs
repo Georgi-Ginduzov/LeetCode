91ef576811559d0b0a19ad9fcbf9f8c878705990
Georgi <123997331+Georgi-Ginduzov@users.noreply.github.com>
2025-07-15 22:01:25 +0300

    Time: 0 ms (100.00%) | Memory: 46.4 MB (36.24%) - LeetSync

---
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cs
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    int max = 0;
+    public int Diameter(TreeNode root)
+    {
+        if(root is null)
+            return -1;
+        var left = Diameter(root.left);
+        var right = Diameter(root.right);
+
+        max = max > (left + right + 2) ? max : (left + right + 2);
+        if(left > right)
+            return 1 + left;
+        return 1 + right;
+    }
+
+    public int DiameterOfBinaryTree(TreeNode root) {
+        Diameter(root);
+        return max;
+    }
+}

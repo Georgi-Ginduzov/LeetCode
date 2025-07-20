c5c1f799932804d512fb9dfee98a9906ce04d091
Georgi <123997331+Georgi-Ginduzov@users.noreply.github.com>
2025-07-20 19:01:43 +0300

    Time: 2 ms (89.02%) | Memory: 52.7 MB (80.49%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cs
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cs
@@ -1,25 +1,23 @@
 public class Solution {
     public int[] FindMissingAndRepeatedValues(int[][] grid) {
-        int[] ans = new int[2];
-        HashSet<int> nums = new HashSet<int>(grid.Length ^ 2);
-        for (int i = 0; i < grid.Length; i++)
+        int[] res = new int[2];
+        bool[] rep = new bool[grid.Length*grid.Length];
+        res[1] = (rep.Length * (rep.Length + 1)) / 2;
+
+        for(int i = 0; i < grid.Length; i++)
         {
-            for (int j = 0; j < grid[i].Length; j++)
+            for(int j = 0; j < grid[i].Length; j++)
             {
-                if (!nums.Add(grid[i][j]))
+                if(rep[grid[i][j] - 1])
+                    res[0] = grid[i][j];
+                else
                 {
-                    ans[0] = grid[i][j];
+                    rep[grid[i][j] - 1] = true;
+                    res[1] -= grid[i][j]; // Calculate missing number
                 }
             }
         }
-        for (int i = 1; i <= nums.Count + 1; i++)
-        {
-            if (!nums.Contains(i))
-            {
-                ans[1] = i;
-                break;
-            }
-        }
-        return ans;
+
+        return res;
     }
 }

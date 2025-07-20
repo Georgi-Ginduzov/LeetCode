9dafa58d5212062f63c2d2d41b97b8611aa85821
Georgi <123997331+Georgi-Ginduzov@users.noreply.github.com>
2025-07-20 19:14:07 +0300

    Time: 0 ms (100.00%) | Memory: 52.1 MB (100.00%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cs
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cs
@@ -1,6 +1,6 @@
 public class Solution {
     public int[] FindMissingAndRepeatedValues(int[][] grid) {
-        int[] res = new int[2]; // Span<int> map = stackalloc int[grid.Length * grid.Length + 1];
+        int[] res = new int[2];
         Span<bool> rep = stackalloc bool[grid.Length*grid.Length + 1];
         res[1] = ((rep.Length - 1) * rep.Length) / 2;
 

d9d087ec2637ac8669b9226899f731fa9901f798
Georgi <123997331+Georgi-Ginduzov@users.noreply.github.com>
2025-08-01 13:53:54 +0300

    Time: 0 ms (100.00%) | Memory: 50.8 MB (42.07%) - LeetSync

--- 617-merge-two-binary-trees/merge-two-binary-trees.cs
+++ 617-merge-two-binary-trees/merge-two-binary-trees.cs
@@ -13,17 +13,24 @@
  */
 public class Solution {
     public TreeNode MergeTrees(TreeNode root1, TreeNode root2) {
-        if (root1 == null & root2 == null) return null;
-        if (root1 == null) return root2;
-        if (root2 == null) return root1;
-
+        if(root1 is null)
+            if(root2 is null)
+                return root1;
+            else
+                return root2;
+        else if(root2 is null)
+            return root1;
+        
         var left = MergeTrees(root1.left, root2.left);
         var right = MergeTrees(root1.right, root2.right);
 
-        if (root1.left != left) root1.left = left;
-        if (root1.right != right) root1.right = right;
-
         root1.val += root2.val;
+        if(left is null)
+            if(right is null)
+                return root1;
+            
+        root1.left = left;
+        root1.right = right;
 
         return root1;
     }

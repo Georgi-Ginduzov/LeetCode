6564f55970f6393c9922cbbe7cc28d332b17ab33
Georgi <123997331+Georgi-Ginduzov@users.noreply.github.com>
2025-08-23 10:12:18 +0300

    Time: 1 ms (53.58%) | Memory: 61.3 MB (41.80%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.cs
+++ 975-range-sum-of-bst/range-sum-of-bst.cs
@@ -1,27 +1,18 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     public int val;
- *     public TreeNode left;
- *     public TreeNode right;
- *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 public class Solution {
     int sum = 0;
-    public int RangeSumBST(TreeNode root, int low, int high) {
-        if(root == null)
-            return sum;
+    public void DFS(TreeNode root, int low, int high)
+    {
+        if(root is null)
+            return;
         
-        RangeSumBST(root.left, low, high);
         if(root.val >= low && root.val <= high)
-            sum += root.val;
-        RangeSumBST(root.right, low, high);
-
+            sum+=root.val;
+        
+        DFS(root.left, low, high);
+        DFS(root.right, low, high);
+    }
+    public int RangeSumBST(TreeNode root, int low, int high) {
+        DFS(root, low, high);
         return sum;
     }
 }

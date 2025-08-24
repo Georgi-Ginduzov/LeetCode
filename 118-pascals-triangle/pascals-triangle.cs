6051efa3f250b122d2a5228bfb97065a01ee74b9
Georgi <123997331+Georgi-Ginduzov@users.noreply.github.com>
2025-08-24 15:21:00 +0300

    Time: 0 ms (100.00%) | Memory: 40.3 MB (49.11%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cs
+++ 118-pascals-triangle/pascals-triangle.cs
@@ -1,30 +1,44 @@
+/**
+Given an integer numRows, return the first numRows of Pascal's triangle.
+    - build a pascals triangle 
+    - return it
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+    - i have to make calculations from the 2nd to n-2nd elements
+        - always 0th and n-1st elements are 1s
+    - calculate a row
+        - get elements from the previous row
+        - ith element = i-1th element + the ith element, from the prev row
+
+
+*/
 public class Solution {
     public IList<IList<int>> Generate(int numRows) {
-        var res = new List<IList<int>>(numRows)
+        var p = new List<IList<int>>(numRows);
+        switch(numRows)
         {
-            new List<int>(1) { 1 }
-        };
-        if (numRows == 1)
-            return res;
-        res.Add(new List<int>(2) { 1, 1 });
-        if (numRows == 2)
-            return res;
-
-        for (int i = 2; i < numRows; i++)
-        {
-            int prev = i - 1;
-            res.Add(new List<int>(i + 1));
-
-            res[i].Add(1);
-
-            for (int j = 1; j < i; j++)
-            {
-                res[i].Add(res[prev][j - 1] + res[prev][j]);
-            }
-
-            res[i].Add(1);
+            case 1:
+                p.Add(new List<int>(1){1});
+                return p;
+            case 2:
+                p.Add(new List<int>(1){1});
+                p.Add(new List<int>(2){1,1});
+                return p;
         }
 
-        return res;
+        p.Add(new List<int>(1){1});
+        p.Add(new List<int>(2){1,1});
+
+        for(int row = 2; row < numRows; row++)
+        {
+            p.Add(new List<int>(row+1));
+            p[row].Add(1);
+            for(int i = 1; i < row; i++)
+            {
+                p[row].Add(p[row-1][i-1] + p[row-1][i]);
+            }
+            p[row].Add(1);
+        }
+        return p;
     }
 }

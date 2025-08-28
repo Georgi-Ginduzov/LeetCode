6ccd0e943c233fad7a90cffbaae53b2b3a116f69
Georgi <123997331+Georgi-Ginduzov@users.noreply.github.com>
2025-08-28 23:13:25 +0300

    Time: 6 ms (44.44%) | Memory: 55.4 MB (25.93%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cs
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cs
@@ -1,30 +1,23 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     public int val;
- *     public TreeNode left;
- *     public TreeNode right;
- *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 public class Solution {
-    HashSet<int> nums = new HashSet<int>();
-    
     public bool FindTarget(TreeNode root, int k) {
-        if(root is null)
-            return false;
+        var s = new HashSet<int>();
+        var q = new Queue<TreeNode>();
+        q.Enqueue(root);
         
-        if(nums.Contains(k-root.val))
-            return true;
-        else
-            nums.Add(root.val);
-        
-        if(FindTarget(root.left, k))
-            return true;
-        return FindTarget(root.right, k);
+        while(q.Any())
+        {
+            var el = q.Dequeue();
+
+            if(s.Contains(el.val))
+                return true;
+            s.Add(k - el.val);
+
+            if(el.left != null)
+                q.Enqueue(el.left);
+            if(el.right != null)
+                q.Enqueue(el.right);
+        }
+
+        return false;
     }
 }

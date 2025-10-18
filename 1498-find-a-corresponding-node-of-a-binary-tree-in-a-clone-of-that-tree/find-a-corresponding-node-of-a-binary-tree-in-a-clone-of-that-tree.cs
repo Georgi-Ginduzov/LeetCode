963f398b1aa956b239d8a6f1de4c7377fde31843
Georgi <123997331+Georgi-Ginduzov@users.noreply.github.com>
2025-10-19 01:11:16 +0300

    Time: 387 ms (69.12%) | Memory: 59.6 MB (44.12%) - LeetSync

--- 1498-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cs
+++ 1498-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cs
@@ -1,20 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+
 public class Solution {
     public TreeNode GetTargetCopy(TreeNode original, TreeNode cloned, TreeNode target) {
-        var tree = new Queue<TreeNode>();
-        tree.Enqueue(cloned);
-
-        while(tree.Any())
-        {
-            var node = tree.Dequeue();
-            if(node.val == target.val)
-                return node;
-            
-            if(node.left != null)
-                tree.Enqueue(node.left);
-            if(node.right != null)
-                tree.Enqueue(node.right);
-        }
-
-        return null;
+        if(cloned is null)
+            return null;
+        if(cloned.val == target.val)
+            return cloned;
+        return GetTargetCopy(original, cloned.left, target) ?? GetTargetCopy(original, cloned.right, target);
     }
 }

6b21310a7308232cd8350398e493667545704271
Georgi <123997331+Georgi-Ginduzov@users.noreply.github.com>
2025-10-18 20:00:58 +0300

    Time: 2 ms (18.58%) | Memory: 50.6 MB (5.13%) - LeetSync

--- 733-flood-fill/flood-fill.cs
+++ 733-flood-fill/flood-fill.cs
@@ -1,54 +1,59 @@
-/*
-    To perform a flood fill:
-
-    Begin with the starting pixel and change its color to color.
-        - no need for checks, just change it
-
-    Perform the same process for each pixel that is directly adjacent (pixels that share a side with the original pixel, 
-    either horizontally or vertically) and shares the same color as the starting pixel.
-        - store steps
-        - Make a step by checking if it is inbound and equal to the original el's value
-        - For each step:
-            - remove it
-            - change its colour 
-        finally return the image
-
-    Keep repeating this process by checking neighboring pixels of the updated pixels and modifying their color 
-    if it matches the original color of the starting pixel.
-
-    The process stops when there are no more adjacent pixels of the original color to update.
-    
-    Return the modified image after performing the flood fill.
-        - Modify the image array
-*/
 public class Solution {
     public int[][] FloodFill(int[][] image, int sr, int sc, int color) {
-        if(image[sr][sc] == color)
+        Span<bool> visited = stackalloc bool[image.Length * image[0].Length];
+        int initialColor = image[sr][sc];
+        if (initialColor == color)
             return image;
-        var q = new Queue<(int row, int col)>();
-        q.Enqueue((sr, sc));
-        var val = image[sr][sc];
-        while(q.Any())
+        
+        var adjacent = new Queue<(int row, int col)>();
+        adjacent.Enqueue((sr, sc));
+        visited[image[sr].Length * sr + sc] = true;
+
+        while(adjacent.Count > 0)
         {
-            var el = q.Dequeue();
-            image[el.row][el.col] = color;
-            
-            int row = el.row;
-            int col = el.col;
-            row--;// Check upside
-            if(row >= 0 && image[row][col] == val)
-                q.Enqueue((row, col));
-            row += 2;// Check down
-            if(row < image.Length && image[row][col] == val)
-                q.Enqueue((row, col));
-            row--; // reset row value
-            col--;// Check left
-            if(col >= 0 && image[row][col] == val)
-                q.Enqueue((row, col));
-            col+=2;// Check right
-            if(col < image[row].Length && image[row][col] == val)
-                q.Enqueue((row, col));
+            var cell = adjacent.Dequeue();
+            image[cell.row][cell.col] = color;
+
+            cell.col++; // go right
+            if(cell.col < image[cell.row].Length 
+                && image[cell.row][cell.col] == initialColor
+                && !visited[image[cell.row].Length * cell.row + cell.col])
+            {
+                adjacent.Enqueue(cell);
+                visited[image[cell.row].Length * cell.row + cell.col] = true;
+            }
+
+            cell.col--; // return left
+            cell.row++; // go down
+            if(cell.row < image.Length 
+                && image[cell.row][cell.col] == initialColor
+                && !visited[image[cell.row].Length * cell.row + cell.col])
+            {
+                adjacent.Enqueue(cell);
+                visited[image[cell.row].Length * cell.row + cell.col] = true;
+            }
+
+            cell.row--; // return up
+            cell.col--; // go left
+            if(cell.col >= 0 
+                && image[cell.row][cell.col] == initialColor
+                && !visited[image[cell.row].Length * cell.row + cell.col])
+            {
+                adjacent.Enqueue(cell);
+                visited[image[cell.row].Length * cell.row + cell.col] = true;
+            }
+
+            cell.col++; // return right
+            cell.row--; // go up
+            if(cell.row >= 0 
+                && image[cell.row][cell.col] == initialColor
+                && !visited[image[cell.row].Length * cell.row + cell.col])
+            {
+                adjacent.Enqueue(cell);
+                visited[image[cell.row].Length * cell.row + cell.col] = true;
+            }
         }
+
         return image;
     }
 }

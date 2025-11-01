b9597993e5fc6688b60da7acc1386cdb35e91f21
Georgi <123997331+Georgi-Ginduzov@users.noreply.github.com>
2025-11-01 20:10:14 +0200

    Time: 1 ms (52.77%) | Memory: 44.2 MB (59.78%) - LeetSync

--- 904-leaf-similar-trees/leaf-similar-trees.cs
+++ 904-leaf-similar-trees/leaf-similar-trees.cs
@@ -1,61 +1,36 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     public int val;
- *     public TreeNode left;
- *     public TreeNode right;
- *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 public class Solution {
-    List<int> leaves1 = new List<int>();
-    List<int> leaves2 = new List<int>();
-
-    public void Leaves(TreeNode root)
-    {
-        if(root is null)
-            return;
-        
-        if(root.left is null && root.right is null){
-            leaves1.Add(root.val);
-            return;
-        }
-        
-        Leaves(root.left);
-        Leaves(root.right);
-    }
-
-    public void LeavesTwo(TreeNode root)
-    {
-        if(root is null)
-            return;
-        
-        if(root.left is null && root.right is null){
-            leaves2.Add(root.val);
-            return;
-        }
-        
-        LeavesTwo(root.left);
-        LeavesTwo(root.right);
-    }
-
     public bool LeafSimilar(TreeNode root1, TreeNode root2) {
-        Leaves(root1);
-        LeavesTwo(root2);
-
-        if(leaves1.Count != leaves2.Count)
-            return false;
-        
-        for(int i = 0; i < leaves1.Count; i++)
+        var leaves1 = new Queue<int>();
+        void CollectLeaves(TreeNode root)
         {
-            if(leaves1[i] != leaves2[i])
+            if(root is null)
+                return;
+            if(root.left is null && root.right is null)
+            {
+                leaves1.Enqueue(root.val);
+            }
+            else
+            {
+                CollectLeaves(root.left);
+                CollectLeaves(root.right);
+            }
+        }
+        CollectLeaves(root1);
+
+        bool CompareRootToQueue(TreeNode root)
+        {
+            if(root is null)
+                return true;
+            if (!CompareRootToQueue(root.left))
                 return false;
+            if(root.left is null && root.right is null)
+            {
+                if(leaves1.Count == 0 || leaves1.Dequeue() != root.val)
+                    return false;
+            }
+            return CompareRootToQueue(root.right);
         }
 
-        return true;
+        return CompareRootToQueue(root2) && leaves1.Count == 0;
     }
 }

e2d9a538858c5242e156f1eae7150b7361a89027
Georgi <123997331+Georgi-Ginduzov@users.noreply.github.com>
2025-11-11 21:26:32 +0200

    Time: 8 ms (38.52%) | Memory: 58.2 MB (41.80%) - LeetSync

--- 463-island-perimeter/island-perimeter.cs
+++ 463-island-perimeter/island-perimeter.cs
@@ -1,84 +1,28 @@
 public class Solution {
     public int IslandPerimeter(int[][] grid) {
-        Span<bool> visited = stackalloc bool[grid.Length * grid[0].Length];
-        int perimeter = 0;
-        var lands = new Queue<(int row, int col)>();
-        var landBeginning = FindFirstLand(grid);
-        if(landBeginning.x is -1)
-            return 0;
-
-        lands.Enqueue(landBeginning);
-        visited[(grid[landBeginning.x].Length * landBeginning.x) + landBeginning.y] = true;
-
-        while(lands.Count > 0)
-        {
-            var neighbour = lands.Dequeue();
-            int waterTiles = 4;
-
-            neighbour.row--; // Check upper cell
-            if(neighbour.row >= 0 && IsLand(grid, neighbour))
-            {
-                waterTiles--;
-                if(!visited[(grid[neighbour.row].Length * neighbour.row) + neighbour.col])
-                {
-                    lands.Enqueue(neighbour);
-                    visited[(grid[neighbour.row].Length * neighbour.row) + neighbour.col] = true;
-                }
-            }
-            
-            neighbour.row++; // Recover original row
-            neighbour.col--; // Check left cell
-            if(neighbour.col >= 0 && IsLand(grid, neighbour))
-            {
-                waterTiles--;
-                if(!visited[(grid[neighbour.row].Length * neighbour.row) + neighbour.col])
-                {
-                    lands.Enqueue(neighbour);
-                    visited[(grid[neighbour.row].Length * neighbour.row) + neighbour.col] = true;
-                }
-            }
-            
-            neighbour.col++; // Recover original col
-            neighbour.row++; // Check downward cell
-            if(neighbour.row < grid.Length && IsLand(grid, neighbour))
-            {
-                waterTiles--;
-                if(!visited[(grid[neighbour.row].Length * neighbour.row) + neighbour.col])
-                {
-                    lands.Enqueue(neighbour);
-                    visited[(grid[neighbour.row].Length * neighbour.row) + neighbour.col] = true;
-                }
-            }
-            
-            
-            neighbour.row--; // Recover original row
-            neighbour.col++; // Check right cell
-            if(neighbour.col < grid[neighbour.row].Length && IsLand(grid, neighbour))
-            {
-                waterTiles--;
-                if(!visited[(grid[neighbour.row].Length * neighbour.row) + neighbour.col])
-                {
-                    lands.Enqueue(neighbour);
-                    visited[(grid[neighbour.row].Length * neighbour.row) + neighbour.col] = true;
-                }
-            }
-            
-            perimeter += waterTiles;
-        }
+        var visited = new bool[grid.Length, grid[0].Length];
         
-        return perimeter;
-    }
+        for(int i = 0; i < grid.Length; i++)
+            for(int j = 0; j < grid[i].Length; j++)
+                if(grid[i][j] == 1)
+                    return (LandPerimeter(i, j));
 
-    private (int x, int y) FindFirstLand(int[][] grid)
-    {
-        for(int row = 0; row < grid.Length; row++)
-            for(int col = 0; col < grid[row].Length; col++)
-                if(grid[row][col] is 1)
-                    return (row, col);
-        return (-1, -1);
-    }
+        return 0;
 
-    // Return if the neighbour is land
-    private bool IsLand(int[][] grid, (int row, int col) newCoords)
-        => grid[newCoords.row][newCoords.col] is 1;
+        int LandPerimeter(int x, int y)
+        {
+            if(x < 0 || x >= grid.Length || y < 0 || y >= grid[x].Length)
+                return 1;
+            
+            if(grid[x][y] == 0)
+                return 1;
+            
+            if(visited[x,y])
+                return 0;
+            
+            visited[x,y] = true;
+
+            return LandPerimeter(x+1, y) + LandPerimeter(x-1, y) + LandPerimeter(x,y+1) + LandPerimeter(x,y-1);
+        }
+    }
 }

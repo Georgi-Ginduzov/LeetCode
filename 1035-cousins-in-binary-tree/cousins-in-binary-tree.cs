bc7235dcaffc5dd333f384092cfd0b03cf844901
Georgi <123997331+Georgi-Ginduzov@users.noreply.github.com>
2025-11-15 01:49:59 +0200

    Time: 0 ms (100.00%) | Memory: 44 MB (72.73%) - LeetSync

--- 1035-cousins-in-binary-tree/cousins-in-binary-tree.cs
+++ 1035-cousins-in-binary-tree/cousins-in-binary-tree.cs
@@ -1,43 +1,32 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     public int val;
- *     public TreeNode left;
- *     public TreeNode right;
- *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 public class Solution {
-    TreeNode xParent;
-    int xDepth;
-    TreeNode yParent;
-    int yDepth;
+    private TreeNode parentX;
+    private TreeNode parentY;
+    private int depthX;
+    private int depthY;
     
-    public void DFS(TreeNode root, int depth, TreeNode parent, int x, int y)
+    private void Dfs(TreeNode root, int x, int y, int depth, TreeNode parent)
     {
         if(root is null)
             return;
         
         if(root.val == x)
         {
-            xParent = parent;
-            xDepth = depth;
+            parentX = parent;
+            depthX = depth;
         }
         else if(root.val == y)
         {
-            yParent = parent;
-            yDepth = depth;
+            parentY = parent;
+            depthY = depth;
         }
 
-        DFS(root.left, depth + 1, root, x, y);
-        DFS(root.right, depth + 1, root, x, y);
-    }
+        Dfs(root.left, x, y, depth+1, root);
+        Dfs(root.right, x, y, depth+1, root);
+    }    
+
     public bool IsCousins(TreeNode root, int x, int y) {
-        DFS(root, 0, root, x, y);
-        return xParent.val != yParent.val && xDepth == yDepth;
+        Dfs(root, x, y, 0, root);
+
+        return parentX.val != parentY.val && depthX == depthY;
     }
 }

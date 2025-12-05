fea0d9350fda0cdf822b7f04cd1b82d8297fa31f
Georgi <123997331+Georgi-Ginduzov@users.noreply.github.com>
2025-12-06 01:24:13 +0200

    Time: 1 ms (100.00%) | Memory: 51.4 MB (75.65%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cs
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cs
@@ -1,16 +1,18 @@
 public class Solution {
     public int[] MinOperations(string boxes) {
-        int[] operations = new int[boxes.Length];
-
+        int movesLeft = 0, movesRight = 0;
+        int boxesLeft = 0, boxesRight = 0;
+        var operations = new int[boxes.Length];
         for(int i = 0; i < boxes.Length; i++)
         {
-            for(int j = 0; j < boxes.Length; j++)
-            {
-                if(j == i)
-                    continue;
+            operations[i] += movesLeft;
+            boxesLeft += boxes[i] - '0';
+            movesLeft += boxesLeft;
 
-                operations[i] += ((boxes[j] - '0') * Math.Abs(j-i));
-            }
+            var otherSideIndex = boxes.Length - 1 - i;
+            operations[otherSideIndex] += movesRight; 
+            boxesRight += boxes[otherSideIndex] - '0';
+            movesRight += boxesRight;
         }
 
         return operations;
